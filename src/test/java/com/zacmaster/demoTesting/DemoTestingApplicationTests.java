9da0b5da65b11fd3831ed9c0c8ccdc4e1b9872c8
Zacarías Samaniego <zacariassamaniego@gmail.com>
2020-01-21 22:35:38 -0300

    first commit

---
+++ src/test/java/com/zacmaster/demoTesting/DemoTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zacmaster.demoTesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
